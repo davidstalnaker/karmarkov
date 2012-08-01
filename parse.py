a3efb5c7f525c0e27e21c8f0d41253e20b9634b7
David Stalnaker <david.stalnaker@gmail.com>
2012-07-31 23:47:20 -0700

    Added ability to get comments from an arbitrary number of posts.

--- parse.py
+++ parse.py
@@ -4,17 +4,23 @@ import json
 import requests
 
 
-def get_topics(subreddit):
+def get_topics(subreddit, prev=None):
   """Gets the URLS of the top topics from the given subreddit in the last year."""
-  url = 'http://reddit.com/r/%s/top.json?sort=top&t=year' % subreddit
-  req = requests.get(url)
+  if prev:
+    prev_text = '&after=%s' % prev
+  else:
+    prev_text = ''
+  url = 'http://reddit.com/r/%s/top.json?sort=top&t=year%s' % (subreddit, prev_text)
+  print(url)
+  req = requests.get(url, headers={'User-Agent': 'karmarkov-bot'})
   if req.status_code != 200:
     print "HTTP error :("
     return []
   urls = []
   for topic in req.json['data']['children']:
     urls += ['http://reddit.com' + topic['data']['permalink']]
-  return urls
+  last_topic = req.json['data']['children'][-1]['data']['name']
+  return urls, last_topic
 
 
 def fill_comments_(post, comments):
@@ -30,7 +36,8 @@ def fill_comments_(post, comments):
 
 def get_comments(url):
   """Gets a list of (comment, karma) tuples for a given reddit URL."""
-  req = requests.get(url + '.json')
+  print(url)
+  req = requests.get(url + '.json', headers={'User-Agent': 'karmarkov-bot'})
   posts = req.json[1]['data']['children']
   comments = []
   for post in posts:
@@ -39,6 +46,13 @@ def get_comments(url):
 
 
 def get_comments_from_sub(subreddit, threads=25):
-  topics = get_topics(subreddit)[0:threads]
+  topics = set()
+  last_topic = None
+  for i in range(0, threads, 25):
+    new_topics, last_topic = get_topics(subreddit, prev=last_topic)
+    if i + 25 > threads:
+      topics.update(new_topics[:threads % 25])
+    else:
+      topics.update(new_topics)
   return reduce (lambda l, c: l + get_comments(c), topics, [])
 
