1f7e70856b62118e10f040a7c8a477c0a20b11d0
Bryan Cuccioli <blc72@cornell.edu>
2012-08-01 00:48:14 -0400

    Aggregate comment-karma tuple list

--- parse.py
+++ parse.py
@@ -13,9 +13,32 @@ def get_topics(subreddit):
     return []
   urls = []
   for topic in req.json['data']['children']:
-    urls += [topic['data']['url']]
+    urls += ['http://reddit.com' + topic['data']['permalink']]
   return urls
 
 
+def fill_comments_(post, comments):
+  if not 'ups' in post:
+    return
+  karma = post['ups'] - post['downs']
+  comments += [(post['body'], karma)]
+  if len(post['replies']) == 0:
+    return
+  for reply in post['replies']['data']['children']:
+    fill_comments_(reply['data'], comments)
 
+
+def get_comments(url):
+  """Gets a list of (comment, karma) tuples for a given reddit URL."""
+  req = requests.get(url + '.json')
+  posts = req.json[1]['data']['children']
+  comments = []
+  for post in posts:
+    fill_comments_(post['data'], comments)
+  return comments
+
+
+def get_comments_from_sub(subreddit, threads=25):
+  topics = get_topics(subreddit)[0:threads]
+  return reduce (lambda l, c: l + get_comments(c), topics, [])
 
